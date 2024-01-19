0b44041b94ae1b7c4ca3fe16dce3aadac9681898
AliaksandraPapko <saschkamihnovez66@gmail.com>
2024-01-19 23:39:28 +0300

    почти сделала дз-10
    форма не хочет отправляться, не могу понять в чем причина

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const form = document.querySelector(`.form`);
+const button = document.querySelector(`#button`);
+const addName = document.querySelector(`#name`);
+const addSecondName = document.querySelector(`#secondName`);
+const addPhone = document.querySelector(`#phone`);
+const addEmail = document.querySelector(`#email`);
+const checkbox = document.querySelector(`#agree`);
 
-  // Здесь твой код
-});
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    async function sendData() {
+        const name = addName.value;
+        const secondName = addSecondName.value;
+        const phone = addPhone.value;
+        const email = addEmail.value;
+        const isOn = checkbox.checked;
+
+
+        const { status, error } = await fetch(`https://polinashneider.space/user`, {
+            method: `POST`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: AliaksandraPapko'
+            },
+            body: JSON.stringify({
+                "name": `${name}`,
+                "secondName": `${secondName}`,
+                "phone": `${phone}`,
+                "email": `${email}`,
+                "agree": `${isOn}`
+            }),
+        })
+
+
+        if (status === 200) {
+            onSuccess(event.target)
+        } else {
+            onError(error)
+        }
+    }
+    button.addEventListener('click', sendData)
+})
+
+
+function toggleLoader() {
+    const loader = document.querySelector(`loader`);
+    loader.classList.toggle('hidden')
+}
+
+function onSuccess(form) {
+    alert('Ваша анкета отправлена');
+    form.reset()
+}
+
+function onError(error) {
+    alert(error.message)
+}
